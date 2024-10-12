e75a2fbcda74eb9007ad7fb041ffe29a96fab2b5
mcombeau <mcombeau@student.42.fr>
2024-10-12 15:36:57 +0200

    remove homebrew install -> install in ansible playbook

--- run_once_install_homebrew.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -f "/home/linuxbrew/.linuxbrew/bin/brew" ]; then
-  echo "Installing Brew"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-  echo "Brew already installed, skipping."
-fi
